b612c9771e0a8c7d4f522d57308bbf6bd163607b
21002624 <akasharul2407@gmail.com>
2024-07-27 23:27:45 +0530

    first commit

--- src/Components/Home/Social.jsx
+++ src/Components/Home/Social.jsx
@@ -3,15 +3,15 @@ import React from 'react'
 const Social = () => {
   return (
     <div className="home__social">
-        <a href="https://www.instagram.com/akzhxx_?igsh=MW44MGlxMWpna3Jtdw==" className="home__social-icon" target="_blank">
+        <a href="https://www.instagram.com/akzhxx_?igsh=MW44MGlxMWpna3Jtdw==" className="home__social-icon" rel="noopener noreferrer" target="_blank">
             <i class="uil uil-instagram"></i>
         </a>
 
-        <a href="https://www.linkedin.com/in/im-akasharul/" className="home__social-icon" target="_blank">
+        <a href="https://www.linkedin.com/in/im-akasharul/" className="home__social-icon" rel="noopener noreferrer" target="_blank">
             <i class="uil uil-dribbble"></i>
         </a>
 
-        <a href="https://github.com/21002624" className="home__social-icon" target="_blank">
+        <a href="https://github.com/21002624" className="home__social-icon" rel="noopener noreferrer" target="_blank">
             <i class="uil uil-github-alt"></i>
         </a>
     </div>
--- src/Components/Scrollup/Scrollup.jsx
+++ src/Components/Scrollup/Scrollup.jsx
@@ -9,7 +9,7 @@ const Scrollup = () => {
         else scrollUP.classList.remove("show-scroll");
     })
   return (
-    <a href="#" className="scrollup">
+    <a href="#" className="scrollup" rel="noopener noreferrer">
         <i className="uil uil-arrow-up scrollup__icon"></i>
     </a>
   )
