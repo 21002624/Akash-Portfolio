22e53f639db85de2ffc5dcda25049cc65845dc64
21002624 <akasharul2407@gmail.com>
2024-07-28 13:50:11 +0530

    first commit

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -37,7 +37,7 @@ const Contact = () => {
                         <i className="bx bx-mail-send contact__card-icon"></i>
 
                         <h3 className="contact__card-title">Email</h3>
-                        <span className="contact__card-data">akasharul2407@gmail.com</span>
+                        <span className="contact__card-data">akasharul2407</span>
 
                         <a href="mailto:akasharul2407@gmail.com.com" className="contact__button">Write me
                             <i className="bx bx-right-arrow-alt contact__button-icon"></i>
